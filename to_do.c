8240702989d2a895c20a007b629e85e1fdea1542
MattMFX <matheus0210@hotmail.com>
2021-05-01 01:23:24 -0300

    update

--- to_do.c
+++ to_do.c
@@ -2,77 +2,55 @@
 #include <stdlib.h>
 #include <time.h>
 
-struct to_do_list{
+struct tarefa{
     long id;
-    char *descricao;
+    char descricao[50];
     struct tm *data;
     enum prioridade {Urgente=1, Alta_prioridade=2, Normal=3, Baixa_prioridade=4} prioridade;
-    char *categoria;
+    char categoria[50];
 };
 
 void exclui_tarefa(long id){
 
 }
 
-void salva_tarefa(struct to_do_list *lista){
-    FILE *bin_ptr = fopen("tarefas.bin", "ab");
+void edita_tarefa(long id){
 
-    fwrite(&lista, sizeof(lista), 1, bin_ptr);
-    
+}
+
+void salva_tarefa(struct tarefa *lista){
+    printf("%s", lista->descricao);
+
+    FILE *bin_ptr = fopen("tarefas.bin", "ab+");
+
+    struct tarefa *tar = (struct tarefa *) malloc(8);
+
+    fwrite(lista, 8, 1, bin_ptr);
+    fread(tar, 8, 1, bin_ptr);
+
+    printf("%s", tar->descricao);
+
     fclose(bin_ptr);
 }
 
 void main(){
-    struct to_do_list *tarefas;
+    /*struct tarefa *tarefas1 = (struct tarefa *) malloc(8);
+    struct tarefa *tarefas2 = (struct tarefa *) malloc(8);
+    struct tarefa *tarefas3 = (struct tarefa *) malloc(8);
+    struct tarefa *tarefas4 = (struct tarefa *) malloc(8);
 
+    tarefas1->descricao = "tarefa";
+    tarefas2->descricao = "tarefa";
+    tarefas3->descricao = "tarefa";
+    tarefas4->descricao = "tarefa";
+
+    salva_tarefa(tarefas1);
+    salva_tarefa(tarefas2);
+    salva_tarefa(tarefas3);
+    salva_tarefa(tarefas4);*/
 }
 
 
-//struct to_do_list *lista cria_tarefa(void)
+//struct tarefa *lista cria_tarefa(void)
 
 //void consulta_tarefas()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*void salva_lista(){
-    //FILE *fptr = fopen("lista.bin", "ab");
-}
-
-void main(){
-    struct to_do_list list;
-    time_t tempo;
-    char *data;
-
-    time(&tempo);
-    list.data = localtime(&tempo);
-    strftime(data, 11, "%d/%m/%Y", list.data);
-
-    printf("Current local time and date: %s", list.data);
-}
-*/
